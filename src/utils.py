913eb9e16070d27b2014ed586da48bf848b71e96
Noel Kuasmapa <kuasmapajackkuasmapa@gmail.com>
2026-05-10 20:40:14 -0700

    update model training file

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,9 @@
-import os 
+import os
 import sys
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
 import dill
-from sklearn.metrics import r2_score
+#from sklearn.metrics import r2_score
 from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -16,4 +16,3 @@ def save_object(file_path,obj):
             
     except Exception as e:
         raise CustomException(e, sys)
-        
